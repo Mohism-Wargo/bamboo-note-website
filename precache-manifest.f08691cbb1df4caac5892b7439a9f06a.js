0df9f69412845d0e7f6cb7cad5feb5e84aa2ae7b
Mohism <hk81206676@163.com>
2022-09-22 18:01:16 +0800

    deploy

---
+++ precache-manifest.f08691cbb1df4caac5892b7439a9f06a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b081d0fd0283aadc0a",
+    "url": "/bamboo-note-website/css/app.24a344ef.css"
+  },
+  {
+    "revision": "28efb51f3ee1520d313b1d55664113f0",
+    "url": "/bamboo-note-website/index.html"
+  },
+  {
+    "revision": "b6b081d0fd0283aadc0a",
+    "url": "/bamboo-note-website/js/app.32a131b4.js"
+  },
+  {
+    "revision": "9aa9ecb5cb17f8d31d67",
+    "url": "/bamboo-note-website/js/chunk-vendors.85a80c90.js"
+  },
+  {
+    "revision": "e109ad6f47fac1bdefa315796ecefc83",
+    "url": "/bamboo-note-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bamboo-note-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bamboo-note-website/precache-manifest.f08691cbb1df4caac5892b7439a9f06a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "bamboo-note"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
